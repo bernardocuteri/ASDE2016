42b331120fb9e6a9a294f1fe1586383af7559e4c
bernardo <cuteri@mat.unical.it>
2016-10-21 11:40:25 +0200

    Slides added

---
+++ SpringAOP/src/fibonacci/Fibonacci.java
@@ -0,0 +1,17 @@
+package fibonacci;
+
+public class Fibonacci {
+
+	private int n;
+	
+	public int compute() {
+		return 0;
+	}
+	
+	//for JUnit tests we don't use the private field n
+	public int getFibonacciGivenN(int n) {
+		return 0;
+	}
+	
+	//getters setters and constructor
+}
---
+++ SpringAOP/src/fibonacci/Main.java
@@ -0,0 +1,11 @@
+package fibonacci;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//load application context
+		//load fibonacci bean
+		//call fibonacci.compute()
+		//fibonacci sequence: 1 1 2 3 5 8 13 21 34...
+	}
+}
